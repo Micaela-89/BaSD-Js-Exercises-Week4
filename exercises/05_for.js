84c4da88241788b6780eb8d1e45b0c5a9d6ead4d
Micaela Casais <casaismicaela@gmail.com>
2021-10-08 19:39:10 -0300

    BASD:Exercise 5(For) done

---
+++ exercises/05_for.js
@@ -0,0 +1,40 @@
+/******************************************************************************************
+a- Crear un array que contenga 5 palabras y recorrer dicho array utilizando un bucle for de
+JavaScript para mostrar una alerta utilizando cada una de las palabras.********************/
+var fruits= ['apple','banana', 'grapes', 'lemon', 'orange'];
+
+for(var i=0; i < 5; i++){
+    alert('Exercise 5.a: ' + fruits[i])
+}
+/******************************************************************************************
+b- Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una
+alerta por cada palabra modificada.*******************************************************/
+
+for(var i=0; i < 5; i++){
+    alert('Exercise 5.b: ' + fruits[i].charAt().toUpperCase() + fruits[i].substring(1));
+}
+
+/*********************************************************************************************
+c- Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto a)
+recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence. Al
+final mostrar una única alerta con la cadena completa.****************************************/
+
+var sentence= ' ';
+
+for(var i=0; i < 5; i++){
+    sentence = sentence +  fruits[i] + ' ';
+}
+alert('Exercise 5.c: ' + sentence);
+
+/*********************************************************************************************
+d- Crear una array vacío y con un bucle for de 10 repeticiones. Llenar el array con el número de
+repetición, es decir que al final de la ejecución del bucle for debería haber 10 elementos dentro
+del array, desde el número 0 hasta al número 9. Mostrar por la consola del navegador el array 
+final (utilizar console.log).*****************************************************************/
+
+var newArray= [ ];
+
+for(var i=0; i < 10; i++){
+    newArray.push(i);
+}
+console.log('Exercise 5.d:' + newArray);
